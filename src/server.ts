7d690457e10f05732d23f3a9bc0a35b2ee1e05fa
Miguel21S <leaismiguelss@gamail.com>
2024-02-26 23:50:40 +0100

    Subindo la creación de roles en server

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
 
 import express, { Application } from 'express';
 import 'dotenv/config';
+import * as controllers from './controllers/controllers';
 
 const app: Application = express();
 const PORT = process.env.PORT || 9998;
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({
         success: true,
         message: 'Hello World!'
     });
 });
+
+app.get('/api/roles', controllers.getRoles);
+app.post('/api/roles', controllers.crearRoles);
+app.put('/api/roles', controllers.updateRoles);
+app.delete('/api/roles', controllers.deleteRoles);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
