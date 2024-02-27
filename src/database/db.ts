cd4f3200f54af47ba80f2795b61ba088f8b721aa
Miguel21S <leaismiguelss@gamail.com>
2024-02-27 23:31:02 +0100

    Subiendo los cambios de los ficheros package.json, service

--- src/database/db.ts
+++ src/database/db.ts
@@ -4,6 +4,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Roles1708988550093 } from './migrations/1708988550093-roles';
 import { Users1709066205859 } from './migrations/1709066205859-users';
+import { Services1709070844619 } from './migrations/1709070844619-services';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -13,7 +14,7 @@ export const AppDataSource = new DataSource({
     password: "2345",
     database: "gestion_de_citas",
     entities: [],
-    migrations:[Roles1708988550093, Users1709066205859],
+    migrations:[Roles1708988550093, Users1709066205859, Services1709070844619],
     synchronize: false,
     logging: false,
 })
