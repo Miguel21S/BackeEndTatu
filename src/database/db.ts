f6a44a5d15c7f9453510fa9f39f3bec1a14d459b
Miguel21S <leaismiguelss@gamail.com>
2024-02-27 23:48:12 +0100

    Creación de las tabla appointments y sus relaciones

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,6 +5,7 @@ import { DataSource } from "typeorm";
 import { Roles1708988550093 } from './migrations/1708988550093-roles';
 import { Users1709066205859 } from './migrations/1709066205859-users';
 import { Services1709070844619 } from './migrations/1709070844619-services';
+import { Appointments1709073378814 } from './migrations/1709073378814-appointments';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -14,7 +15,7 @@ export const AppDataSource = new DataSource({
     password: "2345",
     database: "gestion_de_citas",
     entities: [],
-    migrations:[Roles1708988550093, Users1709066205859, Services1709070844619],
+    migrations:[Roles1708988550093, Users1709066205859, Services1709070844619, Appointments1709073378814],
     synchronize: false,
     logging: false,
 })
---
+++ src/database/migrations/1709073378814-appointments.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class Appointments1709073378814 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "appointments",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "appointments_date",
+                        type: "timestamp",
+                        isNullable: false,
+                    },
+                    {
+                        name: "user_id",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "services_id",
+                        type: "int",
+                        isNullable: false,
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["user_id"],
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    },
+                    {
+                        columnNames: ["services_id"],
+                        referencedTableName: "services",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE"
+                    }]
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("appointments");
+    }
+
+}
