a25096311b6e37afe9428e065b0f5c25ffbc8754
Miguel21S <leaismiguelss@gamail.com>
2024-02-27 22:01:08 +0100

    Configuración de la tabla users y del fichero package.json

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Roles1708988550093 } from './migrations/1708988550093-roles';
+import { Users1709066205859 } from './migrations/1709066205859-users';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
     password: "2345",
     database: "gestion_de_citas",
     entities: [],
-    migrations:[Roles1708988550093],
+    migrations:[Roles1708988550093, Users1709066205859],
     synchronize: false,
     logging: false,
 })
--- src/database/migrations/1709066205859-users.ts
+++ src/database/migrations/1709066205859-users.ts
@@ -1,11 +1,51 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Users1709066205859 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                        length: "255"
+                    },
+                    {
+                        name: "lastname",
+                        type: "varchar",
+                        length: "255"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        length: "255"
+                    },
+                    {
+                        name: "password",
+                        type: "varchar",
+                        length: "255"
+                    },
+                    {
+                        name: "role_id",
+                        type: "number",
+                        isNullable: false
+                    }
+                ]
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
     }
 
 }
