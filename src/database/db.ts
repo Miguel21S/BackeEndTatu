5035ed7712eed64cc0f35673a10928867d9e24f3
Miguel21S <leaismiguelss@gamail.com>
2024-02-28 01:33:47 +0100

    Actualización en la clase db.ts, definiendo los entities

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,6 +6,10 @@ import { Roles1708988550093 } from './migrations/1708988550093-roles';
 import { Users1709066205859 } from './migrations/1709066205859-users';
 import { Services1709070844619 } from './migrations/1709070844619-services';
 import { Appointments1709073378814 } from './migrations/1709073378814-appointments';
+import { Role } from '../models/Role';
+import { User } from '../models/User';
+import { Appointment } from '../models/Appointment';
+import { Service } from '../models/Service';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -14,7 +18,7 @@ export const AppDataSource = new DataSource({
     username: "root",
     password: "2345",
     database: "gestion_de_citas",
-    entities: [],
+    entities: [Role, User, Appointment, Service],
     migrations:[Roles1708988550093, Users1709066205859, Services1709070844619, Appointments1709073378814],
     synchronize: false,
     logging: false,
